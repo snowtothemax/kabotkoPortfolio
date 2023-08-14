f60cee27313657d21f16533a2067b60f90dbadde
snowtothemax <56100786+snowtothemax@users.noreply.github.com>
2023-08-13 21:27:37 -0500

    push icon changes and link to resume

--- src/screens/general.tsx
+++ src/screens/general.tsx
@@ -1,5 +1,5 @@
 export const ResumeLink = () => {
-    return <a href="https://portfolio-content-bucket.s3.us-east-2.amazonaws.com/MaxJohnsonResume.pdf" target="_blank" download>
+    return <a href="https://portfolio-content-bucket.s3.us-east-2.amazonaws.com/MaxJohnsonResume_Aug23.pdf" target="_blank" download>
         Resume
     </a>
 }
