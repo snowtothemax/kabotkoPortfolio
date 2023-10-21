770bc3de1615e347afcfd3dd5d3075d394f6ce51
snowtothemax <56100786+snowtothemax@users.noreply.github.com>
2023-10-21 17:25:39 -0500

    changed resume link

--- src/screens/general.tsx
+++ src/screens/general.tsx
@@ -1,5 +1,5 @@
 export const ResumeLink = () => {
-    return <a href="https://portfolio-content-bucket.s3.us-east-2.amazonaws.com/MaxJohnsonResume_Sep23.pdf" target="_blank" download>
+    return <a href="https://portfolio-content-bucket.s3.us-east-2.amazonaws.com/MaxJohnsonResume_Oct23.pdf" target="_blank" download>
         Resume
     </a>
 }
